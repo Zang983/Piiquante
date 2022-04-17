8e7796eb2ac019c8d4cfe19462fad914198fc998
Kevin KERKAR <kerkar.kevin@gmail.Com>
2022-04-17 10:13:57 +0200

    Gestion des like (1 bug encore connu)

--- Web-Developer-P6-master/backend/controllers/sauce.js
+++ Web-Developer-P6-master/backend/controllers/sauce.js
@@ -42,6 +42,7 @@ Plusieurs pistes :
     - Enchaîné les conditions avec une partie du code qui se répète.
     - Voir s'il est possible comme dans la partie delete, de préparer des requêtes.
     - A chaque interraction, reset la présence dans le tableau de présence des like ou dislike quitte à le remettre une nouvelle fois dans la catégorie ou il était déjà présent.
+    
 */
 exports.likeASauce = (req, res, next) => {
 
@@ -51,86 +52,91 @@ exports.likeASauce = (req, res, next) => {
     Sauce.findOne({ _id: sauceId })
         .then(
             sauce => {
-                let presenceLike=sauce.usersLiked.includes(userId)
-                let presenceDislike =sauce.usersDisliked.includes(userId)
-                console.log(presenceDislike);
-                console.log(valeurLike)
-
-                if (!presenceLike && !presenceDislike) {
-                    if (valeurLike == 1) {
+                let presenceLike = sauce.usersLiked.includes(userId)
+                let presenceDislike = sauce.usersDisliked.includes(userId)
+/*Si l'utilisateur devient neutre*/
+                if (valeurLike === 0) {
+                    if (presenceLike) {
                         Sauce.updateOne({ _id: sauceId }, {
-                            $inc: { likes: 1 },
-                            $push: { usersLiked: userId }
+                            $pull: { usersLiked: userId },
+                            $inc: { likes: -1 }
                         })
-                            .then(() => res.status(201).json({ message: "Vous avez likez cette sauce !" }))
-                            .catch(error => res.status(400).json({ error }))
+                            .then(() => res.status(201).json({ message: "Vous avez décidé d'être neutre, c'est cool !" }))
+                            .catch(error => res.status(500).json({ error: new Error() }))
                     }
-                    if (valeurLike === -1) {
+                    if (presenceDislike) {
                         Sauce.updateOne({ _id: sauceId }, {
-                            $inc: { dislikes: 1 },
-                            $push: { usersDisliked: userId }
+                            $pull: { usersDisliked: userId },
+                            $inc: { dislikes: -1 }
                         })
-                            .then(() => res.status(201).json({ message: "Vous êtes sûr d'avoir bien gouté cette sauce ?" }))
-                            .catch(error => res.status(400).json({ error }))
+                        .then(() => res.status(201).json({ message: "Vous avez décidé d'être neutre, c'est cool !" }))
+                        .catch(error => res.status(500).json({ error: new Error() }))
                     }
                 }
-                if (valeurLike ===0)
-                {
-                    if(presenceLike)
-                    {
-                        Sauce.updateOne({_id : sauceId},{
-                            $inc:{likes:-1},
-                            $pull:{usersLiked : userId}
-                        })
-                        .then(()=> res.status(201).json({message : "Vous avez décidé d'être neutre"}))
-                        .catch(error => res.status(400).json({ error }))
+/*Si l'utilisateur n'est pas neutre*/
+                else {
+                    if (valeurLike === 1) {//s'il like la sauce on vérifie s'il l'avais disliké auparavant
+                        if (presenceDislike) {
+                            Sauce.updateOne({ _id: sauceId }, {
+                                $pull: { usersDisliked: userId },
+                                $inc: {dislikes : -1},
+                                $push:{usersLiked:  userId},
+                                $inc : {likes : 1}
+                            })
+                            .then(() => res.status(201).json({ message: "Vous avez liké une sauce que vous n\'aimiez pas." }))
+                            .catch(error => res.status(500).json({ error: new Error() }))
+                        }
+                        if(!presenceLike)//il like la sauce sans l'avoir dislike auparavant.
+                        {
+                            Sauce.updateOne({_id:sauceId},{
+                                $push:{usersLiked:userId},
+                                $inc:{likes:1}
+                            })
+                            .then(() => res.status(201).json({ message: "Vous avez liké une sauce" }))
+                            .catch(error => res.status(500).json({ error: new Error() }))
+                        }
                     }
-                    else
-                    {
-                        Sauce.updateOne({_id : sauceId},{
-                            $inc:{dislikes:-1},
-                            $pull:{usersDisliked : userId}
-                        })
-                        .then(()=> res.status(201).json({message : "Vous avez décidé d'être neutre"}))
-                        .catch(error => res.status(400).json({ error }))    
+                    if (valeurLike === -1) {
+                        if (presenceLike) {//s'il dislike la sauce on vérifie s'il l'avait liké auparavant
+                            Sauce.updateOne({ _id: sauceId }, {
+                                $pull: { usersLiked: userId },
+                                $inc: {dislikes : 1},
+                                $push:{usersDisliked:  userId},
+                                $inc : {likes : -1}
+                            })
+                            .then(() => res.status(201).json({ message: "Vous avez disliké une sauce que vous aimiez." }))
+                            .catch(error => res.status(500).json({ error: new Error() }))
+                        }
+                        if(!presenceDislike)//il dislike la sauce sans l'avoir like auparavant.
+                        {
+                            Sauce.updateOne({_id:sauceId},{
+                                $push:{usersDisliked:userId},
+                                $inc:{dislikes:1}
+                            })
+                            .then(() => res.status(201).json({ message: "Vous avez liké une sauce" }))
+                            .catch(error => res.status(500).json({ error: new Error() }))
+                        }
                     }
                 }
 
             })
-        .catch()
-
-
+        .catch(error => res.status(500).json({ error }))
 }
 exports.deleteSauce = (req, res, next) => {
-
-    // const query = Sauce.deleteOne();
-    // query.where('_id').gte(req.params.id).exec(()=>
-    // {
-    //     if(!query.error)
-    //     {
-    //         res.status(201).json({message:"Sauce supprimée"})
-    //     }
-    //     else
-    //     {
-    //         res.status(500).json({message:"Problème suppression de la BDD"})
-    //     }
-    // });
-    
-
     Sauce.deleteOne({ _id: req.params.id })
         .then(() => res.status(201).json({ message: "Sauce supprimée de la BDD" }))
-        .catch(error => res.status(500).json({ message: "Problème suppression de la BDD" }))
+        .catch(error => res.status(500).json({ error: new Error() }))
 
 };
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(Sauce => res.status(200).json(Sauce))
-        .catch(error => res.status(404).json({ error: new Error("Sauce non trouvée") }))
+        .catch(error => res.status(404).json({ error: new Error() }))
 
 };
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then(Sauce => res.status(200).json(Sauce))
-        .catch(error => res.status(400).json({ error: "Problème de récupération des sauces." }))
+        .catch(error => res.status(400).json({ error: new Error() }))
 };
 
